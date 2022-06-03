a173f3b64c61d87085c5196388f0ac0212f49330
Djfard <72867584+Djfard@users.noreply.github.com>
2022-06-03 15:51:17 +0200

    Update script.js

--- Jobbcenter - Hemsida/JS/script.js
+++ Jobbcenter - Hemsida/JS/script.js
@@ -18,3 +18,22 @@ counters.forEach((counter) => {
   };
   updateCounter();
 });
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:10,
+  nav:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1000:{
+          items:5
+      }
+  }
+})
+
